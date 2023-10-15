e3e08b8b294aee750a8e53cc926194a1f5e837f2
linkybolt <moviesminister@gmail.com>
2023-10-15 15:00:46 +0530

    icons-update

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -8,7 +8,14 @@ export default function Comments({ slug }: { slug: string }) {
   const [loadComments, setLoadComments] = useState(false)
   return (
     <>
-      {!loadComments && <button onClick={() => setLoadComments(true)}>Load Comments</button>}
+      {!loadComments && (
+        <button
+          className="mt-2 mr-3 rounded-lg border border-primary-500 py-1 px-3 text-sm font-medium uppercase text-primary-500 transition duration-500 ease-in-out hover:bg-primary-500 hover:text-gray-100 dark:hover:text-gray-900"
+          onClick={() => setLoadComments(true)}
+        >
+          Load Comments
+        </button>
+      )}
       {siteMetadata.comments && loadComments && (
         <CommentsComponent commentsConfig={siteMetadata.comments} slug={slug} />
       )}
