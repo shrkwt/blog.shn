32149cf11b4107602c9bba7762027c0f4188306f
linkybolt <moviesminister@gmail.com>
2023-10-15 14:41:15 +0530

    about-page-update

---
+++ components/techStack.tsx
@@ -0,0 +1,40 @@
+export type TechStack =
+  | 'Python'
+  | 'TypeScript'
+  | 'JavaScript'
+  | 'Java'
+  | 'React'
+  | 'NextJS'
+  | 'Svelte'
+  | 'TensorFlow'
+  | 'PyTorch'
+  | 'Streamlit'
+  | 'PostgreSQL'
+  | 'MySQL'
+  | 'MongoDB'
+  | 'Firebase'
+  | 'FastAPI'
+  | 'Docker'
+  | 'Git'
+  | 'Prisma'
+
+export const techStack: TechStack[] = [
+  'Python',
+  'TypeScript',
+  'JavaScript',
+  'Java',
+  'React',
+  'NextJS',
+  'Svelte',
+  'TensorFlow',
+  'PyTorch',
+  'Streamlit',
+  'PostgreSQL',
+  'MySQL',
+  'MongoDB',
+  'Firebase',
+  'FastAPI',
+  'Docker',
+  'Git',
+  'Prisma',
+]
